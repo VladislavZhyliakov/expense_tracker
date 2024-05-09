ade808e722add6bc2436794bd74cc4cf74b82cb7
VladislavZhyliakov <80688590+VladislavZhyliakov@users.noreply.github.com>
2024-05-09 19:08:27 +0300

    Structured project. Added new expense modal window. Implemented expense adding.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -13,23 +14,40 @@ class Expenses extends StatefulWidget {
 
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
-    Expense(title: 'Flutter course', amount: 19.99, date: DateTime.now(), category: Category.food),
-    Expense(title: 'Flutter course2', amount: 9.99, date: DateTime.now(), category: Category.work),
-    Expense(title: 'Flutter course3', amount: 99.99, date: DateTime.now(), category: Category.leisure),
-    Expense(title: 'Flutter course4', amount: 49.99, date: DateTime.now(), category: Category.travel),
-    Expense(title: 'Flutter course5', amount: 19.99, date: DateTime.now(), category: Category.work),
-    Expense(title: 'Flutter course6', amount: 19.99, date: DateTime.now(), category: Category.food),
+    Expense(
+        title: 'Flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(_addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter expense tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(
-            child: ExpensesList(expenses: _registeredExpenses)
-          )
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
       ),
     );
